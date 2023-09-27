ef7c552e1e89b072d757cbc4df20c6ab77fef54c
Yasser <mr.yasser82@gmail.com>
2023-09-27 22:36:48 +0300

    setting up theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,10 +15,25 @@ class AmazonCloneApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Amazon Clone',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+            elevation: 0, iconTheme: IconThemeData(color: Colors.black)),
+        colorScheme:
+            const ColorScheme.light(primary: GlobalVariables.secondaryColor),
+        // useMaterial3: true,
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Hello'),
+        ),
+        body: Column(
+          children: [
+            const Center(child: Text('This is a text')),
+            ElevatedButton(onPressed: () {}, child: const Text('Click'))
+          ],
+        ),
+      ),
     );
   }
 }
